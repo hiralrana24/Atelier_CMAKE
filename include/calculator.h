c800ac42a07898f6f85e357d807130425314a072
hiralrana24 <hiral.rana0910@gmail.com>
2025-12-10 10:08:19 +0100

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -2,3 +2,6 @@ double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+
+/* Nouvelle fonction : carré */
+double _car(double a);
