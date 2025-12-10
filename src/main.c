8e270297e0fcc8fe948fb2b868a6a59dc891dd27
hiralrana24 <hiral.rana0910@gmail.com>
2025-12-10 10:10:27 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
+int main(int argc, char *argv[]) {
+
+    /* Cas des opérations avec 2 nombres : add, sub, mul, div */
+    if (argc == 4) {
+        char* op = argv[1];
+        char* a = argv[2];
+        char* b = argv[3];
+        double r = 0;
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+            printf("%lf", r);
+        } else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+            printf("%lf", r);
+        } else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+            printf("%lf", r);
+        } else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+            printf("%lf", r);
+        } else {
+            printf("Erreur de parametres");
+        }
+
+    /* Cas particulier : opération au carré, 1 seul nombre */
+    } else if (argc == 3) {
+        char* op = argv[1];
+        char* a = argv[2];
+        double r = 0;
+
+        if (strcmp(op, "car") == 0) {
+            r = _car(atof(a));
+            printf("%lf", r);
+        } else {
+            printf("Erreur de parametres");
+        }
+
+    } else {
+        printf("Erreur de parametres");
+    }
+
+    return 0;
 }
 
 		
